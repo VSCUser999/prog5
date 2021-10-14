b1f6567ee89c851d9649cf1667f3fb84d7a8e4f5
VSCUser999 <def8663@gmail.com>
2021-10-15 01:13:18 +0800

    HM20211015_0113

---
+++ prog5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (void){
+    int iNn1,iNn2,iCount1,iCount2;
+    scanf("%d",&iNn1);
+    iNn2 = iNn1-1;
+    for(iCount1 = 0; iCount1 < iNn1; iCount1++){
+        for(iCount2 = 0; iCount2 < iNn1; iCount2++){
+            if(iCount2 < iNn1 && iCount2 >= iNn2){
+                printf("*");              
+            }else {
+                printf(" ");
+            }  
+        }
+        iNn2--;
+        printf("\n");
+   }
+}
